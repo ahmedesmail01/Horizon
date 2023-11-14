957ed43e126f6862ff4dab5a11bbd3f4cbfe018f
ahmedesmail01 <ahmedesmail34180@gmail.com>
2023-11-14 11:28:53 +0200

    add open graph03

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
-import PaginationBar from '@/components/PaginationBar';
-import ProductCard from '@/components/ProductCard';
-import { prisma } from '@/lib/db/prisma';
-import Image from 'next/image';
-import Link from 'next/link';
-import Head from 'next/head'; // Import Head from next/head
-import oImage from "@/app/opengraph-image.png";
+import PaginationBar from '@/components/PaginationBar'
+import ProductCard from '@/components/ProductCard'
+import { prisma } from '@/lib/db/prisma'
+import Image from 'next/image'
+import Link from 'next/link'
 
-interface HomeProps {
-  searchParams: { page: string };
+interface HomeProps{
+  searchParams:{page:string};
 }
 
-export const metadata = {
-  title: "Home page - Horizon",
-  openGraph: {
-    images: [{ url: oImage }],
-  },
-};
 
-export default async function Home({ searchParams: { page = "1" } }: HomeProps) {
+export default async function Home({searchParams:{page ="1"}}:HomeProps) {
   const currentPage = parseInt(page);
   const pageSize = 9;
   const heroItemCount = 1;
@@ -25,24 +17,17 @@ export default async function Home({ searchParams: { page = "1" } }: HomeProps) 
   const totalPages = Math.ceil((totalItemCount - heroItemCount) / pageSize);
 
   const products = await prisma.product.findMany({
-    orderBy: { id: "desc" },
-    skip: (currentPage - 1) * pageSize + (currentPage === 1 ? 0 : heroItemCount),
-    take: pageSize + (currentPage === 1 ? heroItemCount : 0),
-  });
+    orderBy:{id:"desc"},
+    skip:(currentPage - 1 )*pageSize +(currentPage ===1 ? 0 : heroItemCount),
+    take: pageSize + (currentPage === 1 ? heroItemCount : 0)
+  })
 
   return (
     <div className='flex flex-col items-center'>
-      <Head>
-        <title>{metadata.title}</title>
-        {/* Add Open Graph meta tags */}
-        {metadata.openGraph.images.map((image, index) => (
-          <meta key={index} property="og:image" content={`/app/opengraph-image.png`} />
-        ))}
-      </Head>
-
-      {currentPage === 1 && (
-        <div className='hero rounded-xl bg-base-200'>
-          <div className='hero-content flex-col lg:flex-row'>
+      {
+        currentPage === 1 && (
+          <div className='hero rounded-xl bg-base-200'>
+          <div className='hero-content flex-col lg:flex-row' >
             <Image
               src={products[0].imageUrl}
               alt={products[0].name}
@@ -54,21 +39,25 @@ export default async function Home({ searchParams: { page = "1" } }: HomeProps) 
             <div>
               <h1 className='text-5xl font-bold'>{products[0].name}</h1>
               <p className='py-6'>{products[0].description}</p>
-              <Link href={'/products/' + products[0].id} className='btn btn-primary'>
-                See More
-              </Link>
+              <Link
+              href={'/products/'+products[0].id}
+              className='btn btn-primary'
+              >See More</Link>
             </div>
           </div>
         </div>
-      )}
-
+        )
+      }
       <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 my-4'>
-        {(currentPage === 1 ? products.slice(1) : products).map((product) => (
-          <ProductCard product={product} key={product.id} />
-        ))}
+        {
+          (currentPage === 1 ? products.slice(1):products).map((product)=> (
+            <ProductCard product={product} key={product.id} />
+          ))
+        }
       </div>
-
-      {totalPages > 1 && <PaginationBar currentPage={currentPage} totalPages={totalPages} />}
+      {
+        totalPages > 1 && <PaginationBar currentPage={currentPage} totalPages={totalPages}/>
+      }
     </div>
-  );
+  )
 }
